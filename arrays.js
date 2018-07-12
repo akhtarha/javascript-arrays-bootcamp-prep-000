25d44f32db8da6a17e7e5baa33be777f7f2e17af
Hassan A <has_is_ere@hotmail.com>
2018-07-12 11:17:11 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -35,7 +35,8 @@ function removeElementFromBeginningOfArray(arr){
 }
 
 function destructivelyRemoveElementFromEndOfArray(arr){
-  return arr.pop()
+  arr.pop()
+  return arr
 }
 
 function removeElementFromEndOfArray(arr){
